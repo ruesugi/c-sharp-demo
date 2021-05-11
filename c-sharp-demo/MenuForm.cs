9ef67119bc5e2bf86a59bba2622f6c8fca4e7c44
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-11 11:58:01 +0900

    add:メニュー画面を追加した

---
+++ c-sharp-demo/MenuForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_sharp_demo
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserSaveFormButton_Click(object sender, EventArgs e)
+        {
+            using (var f = new UserSaveForm())
+            {
+                f.ShowDialog();
+            }
+        }
+    }
+}
