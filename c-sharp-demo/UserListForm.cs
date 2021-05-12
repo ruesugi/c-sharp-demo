3040968b092cd229325694abf93fbe47727243aa
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-12 11:16:45 +0900

    add:ユーザーリストを画面に表示する機能を追加した

--- c-sharp-demo/UserListForm.cs
+++ c-sharp-demo/UserListForm.cs
@@ -15,6 +15,23 @@ namespace c_sharp_demo
         public UserListForm()
         {
             InitializeComponent();
+            if (System.IO.File.Exists("user_data.csv"))
+            {
+                var lines = System.IO.File.ReadAllLines("user_data.csv", Encoding.UTF8);
+                foreach (var line in lines)
+                {
+                    var data = line.Split(',');
+
+                    UserDataGrid.ColumnCount = 5;
+                    UserDataGrid.Columns[0].HeaderText = "ID";
+                    UserDataGrid.Columns[1].HeaderText = "メール送信";
+                    UserDataGrid.Columns[2].HeaderText = "メールアドレス";
+                    UserDataGrid.Columns[3].HeaderText = "プラン";
+                    UserDataGrid.Columns[4].HeaderText = "有効/無効";
+
+                    UserDataGrid.Rows.Add(data[0], data[1], data[2], data[3], data[4]);
+                }
+            }
         }
     }
 }
